d6c042807ba0e499c836e50db77319148fe5a841
barkhudaryan-arman <armanik147@gmail.com>
2024-07-15 23:19:42 +0300

    Init

---
+++ src/main/java/com/homework26/homework_demo/HomeworkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.homework26.homework_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkDemoApplication.class, args);
+	}
+
+}
